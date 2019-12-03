2afcce671530fde7aa1bf3bd15d11d5a4efb1c46
Elise Navarro <elise.marie.navarro@gmail.com>
2019-12-03 15:05:46 +0100

    created (not complete) Show method and show view

--- app/controllers/tents_controller.rb
+++ app/controllers/tents_controller.rb
@@ -1,10 +1,13 @@
 class TentsController < ApplicationController
 
   def index
+    # Display ALL Tents on index page
     @tents = Tent.all
   end
 
   def show
+    # Show PDP of single Tent using params :id
+    @tent = Tent.find(params[:id])
   end
 
   def new
