aa106c4e963fc49600298900b0c034db5c3fb397
Ivana Piljic <piljic.ivana@gmail.com>
2019-12-03 13:01:59 +0100

    post method and photo uploader

--- app/controllers/tents_controller.rb
+++ app/controllers/tents_controller.rb
@@ -1,5 +1,4 @@
 class TentsController < ApplicationController
-
   def index
   end
 
@@ -7,9 +6,21 @@ class TentsController < ApplicationController
   end
 
   def new
+    @tent = Tent.new
   end
 
   def create
+    @tent = Tent.new(tent_params)
+    if @tent.save
+      redirect_to tent_path(@tent)
+    else
+      render :new
+    end
   end
 
+  private
+
+  def tent_params
+    params.require(:tent).permit(:title, :description, :address, :photo, :price)
+  end
 end
