d7c1ea12aab340f40ebaa3345644ffdab72ed406
Elise Navarro <elise.marie.navarro@gmail.com>
2019-12-06 12:31:56 +0100

    new migration file and update to schema. Add status to booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_133149) do
+ActiveRecord::Schema.define(version: 2019_12_06_110606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_133149) do
     t.bigint "tent_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.index ["tent_id"], name: "index_bookings_on_tent_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
